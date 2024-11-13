e6062e4d0f1f3334bd5301cbba2c50d2edee638d
Karan Yadav <karan342772@gmail.com>
2024-11-14 00:20:20 +0530

    day2

--- src/component/Start2.jsx
+++ src/component/Start2.jsx
@@ -1,13 +1,25 @@
-import React, { Children } from 'react';
-import Parents from '../pages/Parents';
+import React, { Children, useState } from 'react';
 import Speak from '../pages/Speak';
 import Write from '../pages/Write';
-  
+import { BsPencil } from "react-icons/bs";
+import { RiSpeakLine } from "react-icons/ri";
+
+
 const Start2 = () => {
-  
+const [state,setState] = useState(true)  
+const changeState = (x)=>{
+  setState(x)
+}
   return (
     <>
-    <Parents/>
+    <div className='flex h-screen items-center flex-col bg-yellow-200 justify-between py-5 sm:py-10'>
+      <h1 className='text-3xl font-semibold font-mono  text-center '>Translator</h1>
+      {state ? <Speak/>:<Write/>}
+      <div className='flex gap-5'>
+        <button className='bg-black text-white p-2 px-5 rounded-sm hover:bg-white hover:text-yellow-400 trans ease-in duration-200 flex gap-2 items-center justify-center active:bg-white active:text-yellow-400' onClick={()=>{changeState(true)}}>{<RiSpeakLine/>}Speak</button>
+        <button className='bg-black text-white p-2 px-5 rounded-sm hover:bg-white hover:text-yellow-400 trans ease-in duration-200 flex justify-center items-center gap-2 active:bg-white active:text-yellow-400' onClick={()=>{changeState(false)}}>{<BsPencil/>}Write</button>
+      </div>
+    </div>
     </>
   )
 }
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import {languages} from '../Languages'
 
 const Write = () => {
   return (
-    <div>
-      <p>write</p>
+    <div className='h-auto w-screen px-2 gap-4 flex flex-col'>
+      <div className='bg-white rounded-md'>
+        <div className='flex flex-col p-2 gap-2 box-border'>
+          <select name="" id="" className='w-4/12 bg-black text-yellow-500 p-1 rounded-lg'>
+            {languages.map((x)=>{
+             return <option value={x.name} key={x.name}>{x.name}</option>
+            })}
+          </select>
+          <textarea name="" id="" className='px-2 resize-none' placeholder='Translate from'></textarea>
+        </div>
+      </div>
+
+      <div className='bg-white rounded-md'>
+      <div className='flex flex-col p-2 gap-2'>
+        <select name="" id="" className='w-4/12 bg-black text-yellow-500 p-1 rounded-lg'>
+        {languages.map((x)=>{
+             return <option value={x.name} key={x.name}>{x.name}</option>
+            })}
+        </select>
+        <textarea name="" id="" placeholder='Translate to' className='resize-none px-2 outline-none'></textarea>
+      </div>
+      </div>
     </div>
   )
 }
