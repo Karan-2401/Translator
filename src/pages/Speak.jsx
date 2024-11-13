ce393c3224fa14401ee6c554dd8200a24cf195a9
Karan Yadav <karan342772@gmail.com>
2024-11-14 00:56:15 +0530

    day3 begin

--- src/pages/Speak.jsx
+++ src/pages/Speak.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { languages } from '../Languages'
+import { FaPlay } from "react-icons/fa";
 
 const Speak = () => {
   return (
-    <div>
-      speak
+    <div className='w-screen flex items-center justify-center'>
+      <div className='flex flex-col w-11/12 sm:w-4/12 items-center justify-around gap-5 bg-white p-5 rounded-xl shadow-lg'>
+        <select name="" id="" className='w-4/12 bg-black text-yellow-500 p-1 rounded-lg'>
+          {languages.map((x)=>{
+            return <option value={x.name} key={x.name}>{x.name}</option>
+          })}
+        </select>
+        <button className='bg-black text-yellow-400 p-2 rounded-2xl flex items-center justify-center w-10 h-10'>{<FaPlay/>}</button>
+        <select name="" id="" className='w-4/12 bg-black text-yellow-500 p-1 rounded-lg'>
+          {languages.map((x)=>{
+            return <option value={x.name} key={x.name}>{x.name}</option>
+          })}
+        </select>
+      </div>
     </div>
   )
 }
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -3,21 +3,21 @@ import {languages} from '../Languages'
 
 const Write = () => {
   return (
-    <div className='h-auto w-screen px-2 gap-4 flex flex-col'>
-      <div className='bg-white rounded-md'>
-        <div className='flex flex-col p-2 gap-2 box-border'>
+    <div className='h-auto w-screen px-2 gap-4 flex justify-center items-center flex-col'>
+      <div className='bg-white rounded-md w-11/12 sm:w-6/12 shadow-lg'>
+        <div className='flex flex-col p-2 gap-2 box-border sm:h-52'>
           <select name="" id="" className='w-4/12 bg-black text-yellow-500 p-1 rounded-lg'>
             {languages.map((x)=>{
              return <option value={x.name} key={x.name}>{x.name}</option>
             })}
           </select>
-          <textarea name="" id="" className='px-2 resize-none' placeholder='Translate from'></textarea>
+          <textarea name="" id="" className='px-2 resize-none outline-none' placeholder='Translate from'></textarea>
         </div>
       </div>
 
-      <div className='bg-white rounded-md'>
-      <div className='flex flex-col p-2 gap-2'>
-        <select name="" id="" className='w-4/12 bg-black text-yellow-500 p-1 rounded-lg'>
+      <div className='bg-white rounded-md w-11/12 sm:w-6/12 shadow-lg'>
+      <div className='flex flex-col p-2 gap-2 sm:h-52 '>
+        <select name="" id="" className='w-4/12 bg-black text-yellow-500 p-1 rounded-lg '>
         {languages.map((x)=>{
              return <option value={x.name} key={x.name}>{x.name}</option>
             })}
